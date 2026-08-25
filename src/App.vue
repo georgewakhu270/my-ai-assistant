43037276b69e2bc661a6791667a00f25d690d61f
Derrick Wakhu <georgewakhu270@gmail.com>
2026-08-25 12:12:37 +0300

    tailwind works

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
+
 </script>
 
 <template>
-  <HelloWorld />
+  <div class="flex items-center justify-center bg-blue-500 text-white p-4">Hello world</div>
 </template>
