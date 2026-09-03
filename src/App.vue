2b7b64c5eeae02ade3a6b8fe600ac3ffc628aec9
Derrick Wakhu <georgewakhu270@gmail.com>
2026-09-03 18:17:18 +0300

    added pyodide

--- src/App.vue
+++ src/App.vue
@@ -2,8 +2,9 @@
 import { ref } from 'vue';
 import MyResponse from './components/MyResponse.vue'
 import TestUI from './components/TestUI.vue';
+import Python from './components/Python.vue';
 
-const sel_item = ref('ai')
+const sel_item = ref('python');
 
 const changeSelItem = (item: string) => {
   sel_item.value = item;
@@ -12,12 +13,18 @@ const changeSelItem = (item: string) => {
 </script>
 
 <template>
-  <div><button @click="changeSelItem('ai')">AI</button>
-  <button @click="changeSelItem('test ui')">Test UI</button></div>
+  <div>
+    <button @click="changeSelItem('ai')">AI</button>
+    <button @click="changeSelItem('test ui')">Test UI</button>
+    <button @click="changeSelItem('python')">Python</button>
+  </div>
   <div v-if="sel_item === 'ai'">
     <MyResponse />
   </div>
   <div v-else-if="sel_item === 'test ui'">
     <TestUI />
+  </div>
+  <div v-else-if="sel_item === 'python'">
+    <Python/>
   </div>
 </template>
